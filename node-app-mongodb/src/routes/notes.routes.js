50087803efa652d7fd96f05337e1db58f36c7266
cmachado17 <machado.cristian1217@gmail.com>
2020-09-28 14:57:18 -0300

    add routes, controllers, install morgan and render a form for new notes

---
+++ node-app-mongodb/src/routes/notes.routes.js
@@ -0,0 +1,30 @@
+const { Router } = require("express");
+
+const router = Router();
+
+const {
+  renderNoteForm,
+  createNewNote,
+  renderNotes,
+  renderEditForm,
+  updateNote,
+  deleteNote,
+} = require("../controllers/notes.controller");
+
+//new note
+router.get("/notes/add", renderNoteForm);
+
+router.post("/notes/new-note", createNewNote);
+
+//get all note
+router.get("/notes", renderNotes);
+
+//edit notes
+router.get("/notes/edit/:id", renderEditForm);
+
+router.put("/notes/edit/:id", updateNote);
+
+//delete notes
+router.delete("/notes/delete/:id", deleteNote); 
+
+module.exports = router;
--- node-app-mongodb/src/server.js
+++ node-app-mongodb/src/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const morgan = require("morgan");
 
 //inicializaciones
 
@@ -8,28 +9,30 @@ const app = express();
 
 //configuraciones
 
-app.set('port', process.env.PORT || 4000 );
-app.set('views', path.join(__dirname, 'views'))
-app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
-}))
-app.set('view engine', '.hbs');
+app.set("port", process.env.PORT || 4000);
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
 
 //Middlewares
-app.use(express.urlencoded({extended: false}));
-
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 
 //global variables
 
-
-
 //routes
-app.use(require('./routes/index.routes'));
+app.use(require("./routes/index.routes"));
+app.use(require("./routes/notes.routes"));
 
 //static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
