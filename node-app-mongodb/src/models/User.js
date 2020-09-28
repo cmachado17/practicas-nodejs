5909a2baa438b33cde6ddfefbc764a7ee3012c1e
cmachado17 <machado.cristian1217@gmail.com>
2020-09-28 14:22:58 -0300

    add bootstrap, config views, routes

--- node-app-mongodb/src/models/User.js
+++ node-app-mongodb/src/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema(
   {
@@ -13,5 +14,14 @@ const UserSchema = new Schema(
     timestamps: true,
   }
 );
+
+UserSchema.methods.encrypPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+UserSchema.methods.matchPassword = function (password){
+  return await bcrypt.compare(password, this.password);
+}
 
 module.exports = model("User", UserSchema);
--- node-app-mongodb/src/server.js
+++ node-app-mongodb/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 //inicializaciones
@@ -9,6 +10,13 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000 );
 app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}))
+app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -19,9 +27,7 @@ app.use(express.urlencoded({extended: false}));
 
 
 //routes
-app.get('/', (req, res) =>{
-    res.send('Hello world')
-})
+app.use(require('./routes/index.routes'));
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
